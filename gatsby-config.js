cf2f01dc8ab2e952e7fd5948da1e1d3683c73ddc
zacaree <zacharem@gmail.com>
2019-02-06 07:58:32 -0600

    Working with fonts. Now they're working.

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,14 +13,6 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
-    {
-      resolve: "gatsby-plugin-web-font-loader",
-      options: {
-        google: {
-          families: [`Nunito:300`]
-        }
-      }
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
