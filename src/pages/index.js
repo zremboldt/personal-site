4c18bf91345481f93fa79259109c33b1e108aae3
Zac <zacaree@gmail.com>
2019-02-28 07:53:57 -0600

    Put the "about" content into its own component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Logo from "../images/Logo";
 import SplashScreen from "../components/SplashScreen";
 import WorkCard from "../components/WorkCard";
 import ContactCard from "../components/ContactCard";
-import Strength from "../components/Strength";
+import About from "../components/About";
 import SEO from "../components/seo";
 
 export default class IndexPage extends Component {
@@ -62,35 +62,7 @@ export default class IndexPage extends Component {
             <Logo />
           </header>
 
-          <section className="aboutStatement">
-            <h1>
-              Drawing from a strong background in design, I build engaging user focused experiences
-              from concept to final development.
-            </h1>
-          </section>
-
-          <section className="aboutStory">
-            <h3>My Road to Code</h3>
-            <p>
-              I’ve been working in the creative industry for nearly 10 years now as a
-              multidisciplinary graphic designer but I’ve always had that itch to bring my work to
-              life and make it interactive.
-            </p>
-            <p>
-              I dabbled in code here and there but finally decided to go for it in 2016 and become a
-              developer. I’ve never looked back, pouring myself into it daily, always excited for
-              what I’ll build/learn next.
-            </p>
-          </section>
-
-          <section className="aboutStrengths">
-            <h3>Strengths - Gallup's StrengthsFinder</h3>
-            <ul>
-              {strengthsData.map((strength, i) => (
-                <Strength strength={strength} key={i} />
-              ))}
-            </ul>
-          </section>
+          <About data={strengthsData} />
 
           <div className="ctr-work">
             <a href={workData[0].url} className="card cardWork" ref={this.card1}>
