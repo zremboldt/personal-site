6ad96bf10aa20c6547f404af4cc026695bda832a
zacaree <zacharem@gmail.com>
2019-03-03 16:59:34 -0600

    Using Transition rather than Spring because the splashscreen needs to go away after timeout tather than just become invisible...

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,27 +1,53 @@
 import React, { Component } from "react";
-import { Spring, animated } from "react-spring/renderprops";
+import { Transition, animated } from "react-spring/renderprops";
 import Logo from "../images/Logo";
 
 export default class SplashScreen extends Component {
+  state = { showSplashScreen: true };
+
+  componentDidMount() {
+    this.hideSplashScreen();
+  }
+
+  hideSplashScreen = () => {
+    setTimeout(() => {
+      this.setState({ showSplashScreen: false });
+    }, 2800);
+  };
+
   render() {
+    const { showSplashScreen } = this.state;
     return (
-      <Spring
+      // <Spring
+      //   native
+      //   config={{ delay: 2700, tension: 120, friction: 40 }}
+      //   from={{ opacity: 1 }}
+      //   to={{ opacity: 0 }}
+      // >
+      //   {props => (
+      <Transition
         native
-        config={{ delay: 2700, tension: 120, friction: 50 }}
-        from={{ opacity: 1 }}
-        to={{ opacity: 0 }}
+        items={showSplashScreen}
+        initial={{ opacity: 1 }}
+        // from={{ opacity: 1 }}
+        enter={{ opacity: 1 }}
+        leave={{ opacity: 0 }}
       >
-        {props => (
-          <animated.div style={props} className="splashscreen">
-            <div className="splashLogoContainer">
-              <div className="mask" />
-              <div className="mask" />
-              <div className="mask" />
-              <Logo />
-            </div>
-          </animated.div>
-        )}
-      </Spring>
+        {showSplashScreen =>
+          showSplashScreen &&
+          (props => (
+            <animated.div style={props} className="splashscreen">
+              <div className="splashLogoContainer">
+                <div className="mask" />
+                <div className="mask" />
+                <div className="mask" />
+                <Logo />
+              </div>
+            </animated.div>
+          ))
+        }
+      </Transition>
+      // </Spring>
     );
   }
 }
